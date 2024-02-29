db3e3ccd708172603bbd54d57e01a3d95300d3c7
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-02-29 17:21:19 -0300

    add: implementing global CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Tweet } from './components/Tweet'
-
+import './global.css'
 // renderizar => mostrar em tela
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div>
-      <Tweet user="Diego" content="Meu primeiro Tweet"/><br></br>
-      <Tweet user="Carla" content="Hello World"/><br></br>
-      <Tweet user="Guilherme" content="Testando"/><br></br>
+      {/* <Tweet user="Diego" content="Meu primeiro Tweet"/>
+      <Tweet user="Carla" content="Hello World"/> */}
+      <Tweet user="Guilherme" likes={15}> 
+        TestandoOOOOOoo
+      </Tweet>
+      <Tweet user="Diego2" likes={10}>
+        Conteúdo 4
+      </Tweet>
     </div>
   </React.StrictMode>,
 )
