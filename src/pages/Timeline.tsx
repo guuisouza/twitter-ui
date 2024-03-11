7ce00e5fba6841fdf0067c5015792ac6aedf47cf
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-03-11 18:09:33 -0300

    feat: Implementing new tweets with react useState

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,24 +1,42 @@
+import { FormEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 
 import './Timeline.css'
 
-const tweets = [
-    'Meu primeiro tweet',
-    'Teste',
-    'Deu certo tweetar!'
-]
 
 export function Timeline() {
+    //Estado
+    const [newTeet, setNewTeet] =  useState('')
+    const [tweets, setTweets] = useState(
+        [
+            'Meu primeiro tweet',
+            'Teste',
+            'Deu certo tweetar!'
+        ]
+    )
+
+    const createNewTweet = (event: FormEvent) => {
+        event.preventDefault()
+
+        setTweets([newTeet ,...tweets])
+        setNewTeet('')
+    }
+
     return (
         <main className="timeline">
             <Header title={"Home"} />
 
-            <form className="new-tweet-form">
+            <form onSubmit={createNewTweet}  className="new-tweet-form">
                 <label htmlFor="tweet">
                     <img src="https://github.com/guuisouza.png" alt="Guilherme Dilio" />
-                    <textarea id="tweet" placeholder="What's happening?" />
+                    <textarea 
+                        id="tweet" 
+                        placeholder="What's happening?"
+                        value={newTeet}
+                        onChange={(event)=> setNewTeet(event.target.value)} 
+                    />
                 </label>
                 <button type="submit">Tweet</button>
             </form>
@@ -28,7 +46,6 @@ export function Timeline() {
             {tweets.map(tweet => {
                 return <Tweet key={tweet} content={tweet} />
             })}
-
         </main>
     )
 }
