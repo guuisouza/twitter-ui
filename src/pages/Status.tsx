27e41a1556251563c5273931ea9d551b0d1ccb64
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-03-13 14:30:48 -0300

    feat: implementing Status Answer storage

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,28 +1,54 @@
+import { FormEvent, KeyboardEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 
 import './Status.css'
 
-const answers = [
-    'Concordo',
-    'Olha faz sentido',
-    'Parabéns pelo progresso'
-]
 
-export function Status(){
-    return(
+export function Status() {
+    //Estado
+    const [newAnswer, setNewAnswer] = useState('')
+    const [answers, setAnswers] = useState(
+        [
+            'Concordo',
+            'Olha faz sentido',
+            'Parabéns pelo progresso'
+        ]
+    )
+
+    const createNewAnswer = (event: FormEvent) => {
+        event.preventDefault()
+
+        setAnswers([newAnswer, ...answers])
+        setNewAnswer('')
+    }
+
+    const handleHotKeySubmit = (event: KeyboardEvent) =>{
+        if (event.key === 'Enter' && event.ctrlKey || event.metaKey){
+            setAnswers([newAnswer, ...answers])
+            setNewAnswer('')
+        }
+    }
+
+    return (
         <main className="status">
             <Header title={"Tweet"} />
-            
-            <Tweet content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus magnam blanditiis obcaecati nulla, aliquid a quisquam? Repellendus enim molestias ducimus numquam quo sapiente eligendi sit quos nesciunt, vitae quod temporibus? "/>
+
+            <Tweet content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus magnam blanditiis obcaecati nulla, aliquid a quisquam? Repellendus enim molestias ducimus numquam quo sapiente eligendi sit quos nesciunt, vitae quod temporibus? " />
 
             <Separator />
 
-            <form className="answer-tweet-form">
+            <form onSubmit={createNewAnswer} className="answer-tweet-form">
                 <label htmlFor="tweet">
                     <img src="https://github.com/guuisouza.png" alt="Guilherme Dilio" />
-                    <textarea id="tweet" placeholder="Tweet your answer" />
+                    <textarea 
+                        id="tweet" 
+                        placeholder="Tweet your answer" 
+                        value={newAnswer}
+                        onKeyDown={handleHotKeySubmit}
+                        onChange={(event) => {setNewAnswer(event.target.value)}}
+                    />
                 </label>
                 <button type="submit">Answer</button>
             </form>
